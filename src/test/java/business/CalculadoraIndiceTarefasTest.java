42334be623eba0db21051d1f5d6f5732c1fb1873
Thiago Valentim <thiago_valentim1@hotmail.com>
2020-12-11 11:57:37 -0300

    Alteração da descrição do teste

--- src/test/java/business/CalculadoraIndiceTarefasTest.java
+++ src/test/java/business/CalculadoraIndiceTarefasTest.java
@@ -15,7 +15,7 @@ class CalculadoraIndiceTarefasTest {
 
     /**
      * Função <b>getIndiceRealizacaoTarefasComuns()</b>
-     * Deve calcular corretamente o Índice de Realização de Tarefas
+     * Deve calcular corretamente o Índice de Realização de Tarefas de um mês específico
      */
     @Test
     void CT011() {
@@ -38,5 +38,88 @@ class CalculadoraIndiceTarefasTest {
         double indiceRealizacaoTarefas = CalculadoraIndiceTarefas.getIndiceRealizacaoTarefasComuns(moradorDeRepublica, 10, 2020);
 
         assertEquals(0.25d, indiceRealizacaoTarefas, 0.001);
+    }
+
+    /**
+     * Função <b>getIndiceSolucaoReclamacoes()</b>
+     * Deve calcular corretamente o Índice de Solução de Reclamações de um mês específico
+     */
+    @Test
+    void CT019() {
+
+        MoradorDeRepublica moradorDeRepublica = MoradorDeRepublicaFactory.createMoradorDeRepublica();
+        MoradorDeRepublica[] responsaveis = new MoradorDeRepublica[]{ moradorDeRepublica };
+
+        Tarefa[] tarefas = new Tarefa[] {
+                TarefaFactory.createTarefa("RECLAMACAO", "2020-10-20", moradorDeRepublica, responsaveis),
+                TarefaFactory.createTarefa("RECLAMACAO", "2020-10-20", moradorDeRepublica, responsaveis),
+                TarefaFactory.createTarefa("RECLAMACAO", "2020-10-20", moradorDeRepublica, responsaveis),
+                TarefaFactory.createTarefa("RECLAMACAO", "2020-10-20", moradorDeRepublica, responsaveis),
+                TarefaFactory.createTarefa("RECLAMACAO", "2020-10-20", "2020-10-19", moradorDeRepublica, responsaveis),
+        };
+
+        Arrays
+                .stream(tarefas)
+                .iterator()
+                .forEachRemaining(TarefaCollection::addTarefa);
+
+        double indiceRealizacaoTarefas = CalculadoraIndiceTarefas.getIndiceSolucaoReclamacoes(moradorDeRepublica, 10, 2020);
+
+        assertEquals(0.20d, indiceRealizacaoTarefas, 0.001);
+    }
+
+    /**
+     * Função <b>getIndiceRealizacaoTarefasComuns()</b>
+     * Deve calcular corretamente o Índice de Realização de Tarefas de todos os meses
+     */
+    @Test
+    void CT020() {
+
+        MoradorDeRepublica moradorDeRepublica = MoradorDeRepublicaFactory.createMoradorDeRepublica();
+        MoradorDeRepublica[] responsaveis = new MoradorDeRepublica[]{ moradorDeRepublica };
+
+        Tarefa[] tarefas = new Tarefa[] {
+                TarefaFactory.createTarefa("COMUM", "2020-10-20", moradorDeRepublica, responsaveis),
+                TarefaFactory.createTarefa("COMUM", "2020-10-20", "2020-10-19", moradorDeRepublica, responsaveis),
+                TarefaFactory.createTarefa("COMUM", "2020-11-20", moradorDeRepublica, responsaveis),
+                TarefaFactory.createTarefa("COMUM", "2020-11-20", "2020-11-19", moradorDeRepublica, responsaveis),
+        };
+
+        Arrays
+                .stream(tarefas)
+                .iterator()
+                .forEachRemaining(TarefaCollection::addTarefa);
+
+        double indiceRealizacaoTarefas = CalculadoraIndiceTarefas.getIndiceRealizacaoTarefasComuns(moradorDeRepublica);
+
+        assertEquals(0.50d, indiceRealizacaoTarefas, 0.001);
+    }
+
+    /**
+     * Função <b>getIndiceSolucaoReclamacoes()</b>
+     * Deve calcular corretamente o Índice de Solução de Reclamações de todos os meses
+     */
+    @Test
+    void CT021() {
+
+        MoradorDeRepublica moradorDeRepublica = MoradorDeRepublicaFactory.createMoradorDeRepublica();
+        MoradorDeRepublica[] responsaveis = new MoradorDeRepublica[]{ moradorDeRepublica };
+
+        Tarefa[] tarefas = new Tarefa[] {
+                TarefaFactory.createTarefa("RECLAMACAO", "2020-10-20", moradorDeRepublica, responsaveis),
+                TarefaFactory.createTarefa("RECLAMACAO", "2020-10-20", moradorDeRepublica, responsaveis),
+                TarefaFactory.createTarefa("RECLAMACAO", "2020-10-20", "2020-10-19", moradorDeRepublica, responsaveis),
+                TarefaFactory.createTarefa("RECLAMACAO", "2020-11-20", moradorDeRepublica, responsaveis),
+                TarefaFactory.createTarefa("RECLAMACAO", "2020-11-20", "2020-11-19", moradorDeRepublica, responsaveis),
+        };
+
+        Arrays
+                .stream(tarefas)
+                .iterator()
+                .forEachRemaining(TarefaCollection::addTarefa);
+
+        double indiceRealizacaoTarefas = CalculadoraIndiceTarefas.getIndiceSolucaoReclamacoes(moradorDeRepublica);
+
+        assertEquals(0.40d, indiceRealizacaoTarefas, 0.001);
     }
 }
